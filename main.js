d3df8c47075006bb7630688d6e0f39b2261e1e02
leegallagher001 <lee.gallagher.writing@outlook.com>
2025-10-18 20:19:27 +0100

    Made some minor additions to the JavaScript, allowing the "theme" of the page (light or dark) to be stored in local storage, so that when the page is loaded again it remembers which theme was active last time.

--- main.js
+++ main.js
@@ -5,18 +5,35 @@ const themeBtn = document.querySelector('#theme');
 // Functions
 
 const storage = {
-
+    saveTheme(value) {
+        localStorage.setItem('habitsapp.theme', `${value}`);
+    },
+    checkTheme() {
+        return localStorage.getItem('habitsapp.theme')
+    }
 }
+
 
 const ui = {
     theme() {
         themeBtn.classList.toggle('dark');
         const root = document.querySelector(':root');
         root.classList.toggle('dark');
+        themeBtn.classList.contains('dark') 
+          ? storage.saveTheme('dark')
+          : storage.saveTheme('light');
     }
 }
 
 // Event Listeners
+
+// Event: window load
+window.addEventListener('DOMContentLoaded', () => {
+    // Load stored theme
+    const theme = storage.checkTheme();
+    console.log(theme);
+    if (theme === 'dark') ui.theme();
+})
 
 // Event: theme button
 themeBtn.addEventListener('click', ui.theme);
